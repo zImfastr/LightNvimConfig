88d49395362e357f95e371c822be148c42995abd
zImfastr <maxjonas.schrader@gmail.com>
2024-11-09 22:07:04 +0100

    Added Fine-Cmdline

---
+++ init.lua
@@ -0,0 +1,10 @@
+require("core.keymaps")
+require("core.plugins")
+require("core.plugin_config")
+
+
+vim.cmd([[
+set number
+colo cyberdream
+]])
+
---
+++ lua/core/keymaps.lua
@@ -0,0 +1,20 @@
+vim.g.maploader = ','
+vim.g.maplocalloader = ','
+
+vim.opt.cursorline = true
+vim.opt.autoread = true
+vim.opt.backspace = '2'
+vim.opt.autowrite = true
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+
+-- use spaces and tab
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+
+
